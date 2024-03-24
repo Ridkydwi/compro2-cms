e2e59b265ced05e0c854f117a67aa8a55fc804e7
Ridkydwi <ridkydwisulist@gmail.com>
2024-03-24 21:13:53 +0700

    first commit

---
+++ pages/services.js
@@ -0,0 +1,17 @@
+
+import Services from "@/sections/Sevices"
+import Head from "next/head"
+import React from "react"
+
+const services = () => {
+  return (
+    <>
+      <Head>
+        <title>Team - Furnicore</title>
+      </Head>
+      <Services/>
+    </>
+  )
+}
+
+export default services
